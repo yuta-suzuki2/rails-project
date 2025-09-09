c5937a2d5cd04bb1f96ca7ac583c6d1122c19f91
yuta-suzuki2 <yuta.suzuki2@dts-insight.co.jp>
2025-09-09 16:58:14 +0900

    lint修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index]
+      resources :users, only: [ :index ]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  TODO: Nuxt連携確認後にテストを有効化する
+  # TODO: Nuxt連携確認後にテストを有効化する
   # test "should get index" do
   #   get api_v1_users_url
   #   assert_response :success
